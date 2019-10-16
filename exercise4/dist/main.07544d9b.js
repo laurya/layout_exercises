d8d0c8daa9c8f17bfda4e23b76e136f4ad9e1650
laury <laury46@gmail.com>
2019-10-16 13:30:13 +0200

    g

--- exercise4/dist/main.07544d9b.js
+++ exercise4/dist/main.07544d9b.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55975" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64686" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
